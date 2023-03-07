af34bc0a2e7e4946da04ad2a5fa46c667bbceead
Arthur <arthur@msapps.mobi>
2023-03-07 18:55:56 +0200

    XCFramework version 1.0.0

---
+++ TopCard.xcframework/ios-arm64/TopCard.framework/Headers/TopCard.h
@@ -0,0 +1,18 @@
+//
+//  TopCard.h
+//  TopCard
+//
+//  Created by Arthur on 25/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TopCard.
+FOUNDATION_EXPORT double TopCardVersionNumber;
+
+//! Project version string for TopCard.
+FOUNDATION_EXPORT const unsigned char TopCardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TopCard/PublicHeader.h>
+
+
